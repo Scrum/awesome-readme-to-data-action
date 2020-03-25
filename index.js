e6864377deb28a0899d7d5bcdbdfb2a54cbbdbc5
Ivan Demidov <Scrum@list.ru>
2020-03-25 17:52:11 +0300

    try: show readme file data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 Toolkit.run(async tools => {
-  const data = fs.readFileSync(path.resolve('readme.md'));
+  const data = fs.readFileSync(path.resolve('./readme.md'));
   console.dir({data})
   tools.log.success('Label successfully applied. Have a nice day!')
 }, {
