417391b87fa7d8af0f3298c8f27a84d38ca7806b
Ivan Demidov <Scrum@list.ru>
2020-03-25 16:19:26 +0300

    feat: first try action

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const { Toolkit } = require('actions-toolkit')
+
+Toolkit.run(async tools => {
+  console.dir({tools})
+  // tools.log.success('Label successfully applied. Have a nice day!')
+}, {
+  event: [
+    'pull_request.opened',
+    'issues.opened'
+  ],
+  secrets: [
+    'GITHUB_TOKEN'
+  ]
+})
