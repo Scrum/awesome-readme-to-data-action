5ac9cb5d7448aa1739c85df97f61dee110749f6c
Ivan Demidov <Scrum@list.ru>
2020-03-26 12:58:41 +0300

    fix: create folder and format data

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const { Toolkit } = require('actions-toolkit');
 const awesomeReadmeToData = require('awesome-readme-to-data');
+const makeDir = require('make-dir');
 
 
 Toolkit.run(async tools => {
   const readmeFileName = 'readme.md';
-  const dataFileName = './data.json';
+  const dataFileName = path.resolve(path.join(tools.workspace, './data/data.json'));
   const contents = tools.getFile(readmeFileName);
   const data = await awesomeReadmeToData(contents);
-  fs.writeFileSync(path.resolve(path.join(tools.workspace, dataFileName)), data);
+  makeDir.sync(path.dirname(dataFileName));
+  fs.writeFileSync(dataFileName, JSON.stringify(data));
 }, {
   event: [
     'pull_request.opened',
