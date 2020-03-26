1ff22d2c1526ca23219b8854e02b99620f085940
Ivan Demidov <Scrum@list.ru>
2020-03-26 10:40:27 +0300

    feat: add logic

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
-const { Toolkit } = require('actions-toolkit');
 const fs = require('fs');
 const path = require('path');
+const { Toolkit } = require('actions-toolkit');
+const awesomeReadmeToData = require('awesome-readme-to-data');
+
 
 Toolkit.run(async tools => {
-  const contents = tools.getFile('readme.md')
-  console.dir({contents})
-  tools.log.success('Label successfully applied. Have a nice day!')
+  const readmeFileName = 'readme.md';
+  const dataFileName = 'data.json';
+  const contents = tools.getFile(readmeFileName);
+  const data = await awesomeReadmeToData(contents);
+  fs.writeFileSync(dataFileName, data);
+  await tools.runInWorkspace('git', ['add', 'data.json']);
+  await tools.runInWorkspace('git', ['-m', '"feat: added new data"']);
+  await tools.runInWorkspace('git', ['push']);
 }, {
   event: [
     'pull_request.opened',
